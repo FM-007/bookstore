01df3f9da9bbdf471d46971e43902574c74add3d
Felipe Moreira <felipe.g.silva@capgemini.com>
2022-09-11 14:38:13 -0300

    Created Author DTO class

---
+++ bookstore/src/main/java/com/felipemoreira/bookstore/domain/dto/AuthorDto.java
@@ -0,0 +1,26 @@
+package com.felipemoreira.bookstore.domain.dto;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthorDto {
+
+    private Long id;
+
+    @NotNull
+    @NotEmpty
+    @Size(max = 255)
+    private String name;
+
+    @NotNull
+    @Max(120)
+    private Integer age;
+}
