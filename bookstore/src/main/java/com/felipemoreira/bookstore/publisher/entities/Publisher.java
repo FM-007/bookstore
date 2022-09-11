1a89b96c07d0654d8101106cfdaded0ffffcb1d6
Felipe Moreira <felipe.g.silva@capgemini.com>
2022-09-10 22:37:54 -0300

    Created model mappings

---
+++ bookstore/src/main/java/com/felipemoreira/bookstore/publisher/entities/Publisher.java
@@ -0,0 +1,37 @@
+package com.felipemoreira.bookstore.publisher.entities;
+
+import static javax.persistence.FetchType.LAZY;
+import static javax.persistence.GenerationType.IDENTITY;
+
+import com.felipemoreira.bookstore.books.entities.Books;
+import java.time.LocalDate;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "PUBLISHER")
+public class Publisher {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @Column(name = "PUBLISHER_NAME", nullable = false, unique = true)
+    private String name;
+
+    @Column(name = "PUBLISHER_CODE", nullable = false, unique = true, length = 100)
+    private String code;
+
+    @Column(name = "FUNDATION_DATE", columnDefinition = "TIMESTAMP")
+    private LocalDate foudationDate;
+
+    @OneToMany(mappedBy = "publisher", fetch = LAZY)
+    private List<Books> books;
+}
