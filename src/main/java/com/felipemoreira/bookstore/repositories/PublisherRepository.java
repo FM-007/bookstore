7b9765b482530d18fdac38f35edb20add52f7c2e
Felipe Moreira <felipe.g.silva@capgemini.com>
2022-11-11 11:45:05 -0300

    Developed publisher creation feature.

--- src/main/java/com/felipemoreira/bookstore/repositories/PublisherRepository.java
+++ src/main/java/com/felipemoreira/bookstore/repositories/PublisherRepository.java
@@ -1,10 +1,12 @@
 package com.felipemoreira.bookstore.repositories;
 
 import com.felipemoreira.bookstore.entities.Publisher;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PublisherRepository extends JpaRepository<Publisher, Long> {
 
+    Optional<Publisher> findByNameOrCode(String name, String code);
 }
--- src/test/java/com/felipemoreira/bookstore/utils/JsonConversionUtils.java
+++ src/test/java/com/felipemoreira/bookstore/utils/JsonConversionUtils.java
@@ -9,14 +9,14 @@ import com.felipemoreira.bookstore.domain.dto.AuthorDto;
 
 public class JsonConversionUtils {
 
-    public static String asJsonString(AuthorDto authorDto) {
+    public static String asJsonString(Object object) {
         try {
             ObjectMapper mapper = new ObjectMapper();
             mapper.configure(FAIL_ON_UNKNOWN_PROPERTIES, false);
             mapper.configure(WRITE_DATES_AS_TIMESTAMPS, false);
             mapper.registerModules(new JavaTimeModule());
 
-            return mapper.writeValueAsString(authorDto);
+            return mapper.writeValueAsString(object);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
